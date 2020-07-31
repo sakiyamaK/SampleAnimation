1ac57cd2dda74e706c533daf9537b73d2ce7b13b
sakiyamaK <sakiyama.k@gmail.com>
2020-08-01 01:13:06 +0900

    animation2

---
+++ SampleAnimation/Animation2/Animation2ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  Animation2ViewController.swift
+//  SampleAnimation
+//
+//  Created by sakiyamaK on 2020/08/01.
+//  Copyright © 2020 sakiyamaK. All rights reserved.
+//
+
+import Foundation
